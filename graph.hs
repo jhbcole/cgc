81336e1b9fa55567930974d057387e2735a7e64d
John Cole <jhbcole@gmail.com>
2013-09-10 23:43:53 -0400

    Builds a graph into a Map using the Vertex as a key and the neighbors as a value. Also SEO works (I think).

--- graph.hs
+++ graph.hs
@@ -11,15 +11,15 @@ import qualified Data.Map as Map
 import qualified Data.List as List
 import qualified Data.Graph as Graph
 import qualified Data.Heap as PQ
-
+import qualified Data.Maybe as Mb
 
 type Vertex = Int
 type Edge = (Int,Int)
 type Edges = [Edge]
-type Graph = Map Vertex [Vertex]
+type Graph = Map.Map Vertex [Vertex]
 
 {-
-Graph - adjacency list,
+Graph - function from Vertex to list of Vertices.
 buildgraph, takes in list of vertices, and list of edges and builds graph
 list of edges may contain both (1,2) and (2,1) for an edge
 -}
@@ -29,7 +29,7 @@ buildgraph v e = let e' = List.union (e) (map (\(x,y)->(y,x)) e)
                      e'' = tabulate (\n -> filter (\(x,y) -> x==n) e') (length v)
                      e''' = (map (map (\(x,y)->y)) e'')
                  in
-                  fromList(tabulate (\x -> e''' !! x) (length(e''')))
+                  Map.fromList(tabulate (\x -> (x,(e''' !! x))) (length(e''')))
 
 -- maxV : Returns the max numbered vertex in of an Edge
 maxV :: Edge -> Int
@@ -40,38 +40,29 @@ maxV (x,y) = if x > y then x else y
 tabulate :: (Vertex -> a) -> Vertex -> [a]
 tabulate f n = map f [0..(n-1)]
 
--- isChordal : Returns true if the graph is chordal of not
-isChordal :: Graph -> Bool
-isChordal g = True
-
--- cycle : Compute a list of cycles in the graph (each cycle is the represented
---         as a list containing the vertices in the cycle) 
-cycle :: Graph -> [[ Vertex ]]
-cycle g = tabulate (\x -> dfs g x) (length g)
-
--- dfs : Given a graph and a vertex, run DFS on the graph and return the visited
---       vertices in a list
-dfs :: Graph -> Vertex -> [ Vertex ]
-dfs g v = [v]
-
 -- nghbr : Given a graph and a vertex, return a list of the neighboring vertices
 nghbr :: Graph -> Vertex -> [ Vertex ]
-nghbr g v =  g !! v
+nghbr g v = g Map.! v
+            --in if Mb.isJust l then Mb.fromJust l else []
 
-seo g =
-  let
-    verts = -- all verts from g
-    weights = PQ.fromList (map (\x -> (0,x)) verts
-  in reverse (seo' g weights [])
-   
+isnghbr :: Graph -> Vertex -> Vertex -> Bool
+isnghbr g v1 v2 = v2 `elem` (nghbr g v1)
+
+seo :: Graph -> [ Vertex ]
+seo g = let verts = tabulate (\x -> x) (Map.size g)
+            weights = PQ.fromAscList (map (\x -> (0,x)) verts) 
+        in
+         List.reverse (seo' g weights [])
+
+seo' :: Graph -> PQ.MaxHeap (Vertex,Vertex) -> [ Vertex ] -> [ Vertex ] 
 seo' g weights l =
   case PQ.view weights of
     Nothing -> l
     Just ((_, v), weights') ->
       let
-        (left, right) = PQ.partition (\(_, v') -> {- is neighbor of v -}) weights'
+        (left, right) = PQ.partition (\(_, v') -> isnghbr g v v'{- is neighbor of v -}) weights'
         left' = PQ.fromList (map (\(prio, val) -> (prio + 1, val)) (PQ.toList left))
         w = PQ.union left' right
-     in seo' w (v : l)
+     in seo' g w (v : l)
       
        
